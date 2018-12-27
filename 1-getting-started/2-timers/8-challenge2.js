32b80e0a699e1fe7c50f73563ca465fc84d6ea3b
Krish <krishc8@hotmail.com>
2018-12-27 02:22:28 -0500

    done challenge 2

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,15 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+var count=0;
+var b = setInterval(
+    () => {
+        console.log('Hello World');
+        count++;
+        if (count >= 5) {
+            clearInterval(b)
+            console.log("Done")
+        }
+        },
+    1000
+);
